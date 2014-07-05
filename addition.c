f559ad705aec5df459187507c6229a9f94dfd669
Satej_khandeparkar <Satejk5@gmail.com>
2014-07-05 16:59:57 +0530

    simple program in c

---
+++ addition.c
@@ -0,0 +1,22 @@
+// PPOJECT_GENE
+//TESTING GIT HUB
+
+// This program adds two numbers
+#include<stidio.h>
+
+int main()
+{
+	int a , b , c ; //declaring two variables
+
+	printf("ENTER TWO NUMBERS \n"); // get the input
+	scanf("%d %d ",&a,&b); // store it in a and b
+
+	c = a + b; //add two numbers
+
+	printf("THE SUM IS %d \n", c); //print the output on a new line
+
+
+
+	return 0;
+
+}
