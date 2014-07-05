c8c056b3f22f4a60f8e24ba1d9de230768111708
Satej_khandeparkar <Satejk5@gmail.com>
2014-07-05 17:00:44 +0530

    program to compare two numbers

---
+++ greater_of_two.c
@@ -0,0 +1,34 @@
+// project_stark
+//TESTING GIT HUB
+
+// This program finds the greater of the two numbers
+
+#include<stidio.h>
+
+int main()
+{
+	int a , b  ; //declaring two variables
+
+	printf("ENTER TWO NUMBERS \n"); // get the input
+	scanf("%d %d ",&a,&b); // store it in a and b
+
+	if(a > b)
+	{
+		printf(" %d is greater \n",a);
+
+	}
+	else
+	{
+
+		printf("%d is greater \n",b)
+
+	}
+	
+
+	
+
+
+
+	return 0;
+
+}
