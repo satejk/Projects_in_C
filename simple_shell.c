ff9407533e9e7df7e85e7487da4cd0cbd3b8d281
Satej Khandeparkar <satejk5@gmail.com>
2015-08-05 00:06:21 +0200

    simple shell

--- simple_shell.c
+++ simple_shell.c
@@ -2,7 +2,7 @@
 #include<sys/types.h>
 #include<sys/wait.h>
 #include<unistd.h>
-
+#include<string.h>
 #define BUFSIZE 128
 
 int main(void){
@@ -17,14 +17,31 @@ int main(void){
 
 	for(;;){
 
+		printf("simplesh: ");
+		if (fgets(command,sizeof(command),stdin) == NULL)
+		{
+			printf(" null \n");
+
+			return 0;
+		}
+
+		command[strlen(command) - 1] = '\0';
+		if((pid = fork()) == 0){
+
+			execlp(command,command,0);
 
 
 
+		}
 
+		while(wait(&status) !=pid){
+
+				continue;
+
+		}
+
+		printf("invalid command \n");
 	}
-
-
-
 
 
 
