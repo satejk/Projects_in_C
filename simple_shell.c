60e4ceaddf1d7bbd01e611b84c8fa26374339128
Satej Khandeparkar <satejk5@gmail.com>
2015-08-04 23:51:59 +0200

    simple shell

---
+++ simple_shell.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<unistd.h>
+
+#define BUFSIZE 128
+
+int main(void){
+
+
+	char command[BUFSIZE];
+
+	int status;
+
+	pid_t pid;
+
+
+	for(;;){
+
+
+
+
+
+	}
+
+
+
+
+
+
+}
